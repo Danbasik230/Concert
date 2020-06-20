37b81edd34f71b1f7a6da7d3839b8d86675ae664
Danbasik <54821904+Danbasik230@users.noreply.github.com>
2020-06-20 22:15:59 +0600

    Add files via upload

---
+++ project/README.md
@@ -0,0 +1,1 @@
+# ConcertD
---
+++ project/scripts/script.js
@@ -0,0 +1,6 @@
+$(".menu__list").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 3000);
+});
